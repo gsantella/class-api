1af857a3ed201c39993198df423b65cfa4ff411b
clynch37 <camlynch314@gmail.com>
2026-09-14 16:21:14 -0400

    added POST PUT DELETE, playing with adding json

--- src/routes/cameron/routes.js
+++ src/routes/cameron/routes.js
@@ -18,35 +18,26 @@ router.get("/", (req, res) => {
 
 router.get("/card/blb/244", (req, res) => {
     res.send("I will send bumbleflowers-sharepot")
-    //name = Bumbleflower's Sharepot
-    //cost = {2}
-    //type = artifact
-    //rules =   When this artifact enters, create a Food token. (It’s an artifact with “{2}, {T}, Sacrifice this token: You gain 3 life.”)
-    //          {5}, {T}, Sacrifice this artifact: Destroy target nonland permanent. Activate only as a sorcery.
-    //flavor = “I may have overestimated the portions.”
-    //illustrator =  J.P. Targete
-    //bool legality[] = [true, true, true, true, true, true, true, true, true, true, true, false]
 });
 
 router.get("/card/blc/3", (req, res) => {
     res.send("I will send ms-bumbleflower")
-    //name = Ms. Bumbleflower
-    //cost = {1}{G}{W}{B}
-    //type = Legendary Creature — Rabbit Citizen 
-    //rules =   Vigilance
-    //          Whenever you cast a spell, target opponent draws a card. Put a +1/+1 counter on target creature. It gains flying until end of turn. If this is the second time this ability has resolved this turn, you draw two cards.
-    //power = 1
-    //toughness = 5
-    //illustrator = Marta Nael
-    //bool legality[] = [false, false, false, true, true, true, false, false, false, false, false, false, false]
 });
 
 router.get("/card", (req, res) => {
     res.send("I should route to a card search")
 });
 
-router.get("/barbeque", (req, res) => {
-    res.send("ribs")
+router.post('/post', (req, res) => {
+  res.send('I will be a POST');
+});
+
+router.put('/put', (req, res) => {
+  res.send('I will be a PUT.');
+});
+
+router.delete('/delete', (req, res) => {
+  res.send('I will be a DELETE.');
 });
 
 export default router;
