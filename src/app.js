7384e15ccf8b72de03560d89c8d22da29b37bf37
gsantella <gsantella@southhills.edu>
2026-09-14 14:44:09 -0400

    initial commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import austinRoutes from './routes/austin/routes.js';
+import cameronRoutes from './routes/cameron/routes.js';
+import deandreRoutes from './routes/deandre/routes.js';
+import destinyRoutes from './routes/destiny/routes.js';
+import elijahRoutes from './routes/elijah/routes.js';
+import guidoRoutes from './routes/guido/routes.js';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api/austin', austinRoutes);
+app.use('/api/cameron', cameronRoutes);
+app.use('/api/deandre', deandreRoutes);
+app.use('/api/destiny', destinyRoutes);
+app.use('/api/elijah', elijahRoutes);
+app.use('/api/guido', guidoRoutes);
+ 
+export default app;
