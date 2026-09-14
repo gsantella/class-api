6ce273d582ff8839d1f4d2e57d299d088a13c5ad
Wade052 <drewade057@gmail.com>
2026-09-14 16:19:50 -0400

    Refactor team management routes to send responses directly

--- src/routes/deandre/routes.js
+++ src/routes/deandre/routes.js
@@ -15,20 +15,24 @@ app.get("/pokemon/:id", (req, res) => {
 
 //#region Team Management
 app.get("/pokemon/team/:UserId", (req, res) => {
-    // this will return a team of 6 pokemon the user selected
+    res.send("This will return a team of 6 pokemon the user selected");  
+  // this will return a team of 6 pokemon the user selected
 });
 
 app.post("/pokemon/team/:UserId", (req, res) => {
+  res.send("This will create a userID and a team of 6 pokemon the user selected");
     // this will create a userID and a team of 6 pokemon the user selected
 });
 
 app.put("/pokemon/team/:UserId/:id", (req, res) => {
+  res.send("This will add a new pokemon the user selected to the team of 6 pokemon");
     // this will add a new pokemon the user selected to the team of 6 pokemon
 });
 
 app.delete("/pokemon/team/:UserId/:slot", (req, res) => {
+  res.send("This will delete a pokemon the user selected from the team of 6 pokemon");
     // this will delete a pokemon the user selected from the team of 6 pokemon
 });
 //#endregion
 
-export default router;
+export default app;
