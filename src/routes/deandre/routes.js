6ba8d1c8bb9c491a175047832e0d8afdf5cc5ddc
Wade052 <drewade057@gmail.com>
2026-09-15 13:40:02 -0400

    Refactor routes to use consistent router variable and improve variable naming

--- src/routes/deandre/routes.js
+++ src/routes/deandre/routes.js
@@ -1,38 +1,39 @@
 import { Router } from 'express';
+import pokemon from "./pokemon.json" with { type: "json" };
 
-const app = Router();
+const router = Router();
 
-app.get("/", (req, res) => res.send("Pokemon"));
+router.get("/", (req, res) => res.send("Pokemon"));
 
-app.get("/pokemon/:id", (req, res) => {
+router.get("/pokemon/:id", (req, res) => {
     const { id } = req.params;
-    const pokemon = pokemon.find((p) => p.id === id);
-    if (!pokemon) {
+    const foundPokemon = pokemon.find((p) => p.id === id);
+    if (!foundPokemon) {
         return res.status(404).send("Pokemon not found");
     }
-    res.json(pokemon);
+    res.json(foundPokemon);
 });
 
 //#region Team Management
-app.get("/pokemon/team/:UserId", (req, res) => {
+router.get("/pokemon/team/:UserId", (req, res) => {
     res.send("This will return a team of 6 pokemon the user selected");  
   // this will return a team of 6 pokemon the user selected
 });
 
-app.post("/pokemon/team/:UserId", (req, res) => {
+router.post("/pokemon/team/:UserId", (req, res) => {
   res.send("This will create a userID and a team of 6 pokemon the user selected");
     // this will create a userID and a team of 6 pokemon the user selected
 });
 
-app.put("/pokemon/team/:UserId/:id", (req, res) => {
+router.put("/pokemon/team/:UserId/:id", (req, res) => {
   res.send("This will add a new pokemon the user selected to the team of 6 pokemon");
     // this will add a new pokemon the user selected to the team of 6 pokemon
 });
 
-app.delete("/pokemon/team/:UserId/:slot", (req, res) => {
+router.delete("/pokemon/team/:UserId/:slot", (req, res) => {
   res.send("This will delete a pokemon the user selected from the team of 6 pokemon");
     // this will delete a pokemon the user selected from the team of 6 pokemon
 });
 //#endregion
 
-export default app;
+export default router;
