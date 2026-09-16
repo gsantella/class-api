c546a7b12cc79c1e6f8ab0c2c987c71e7cd9e5be
gsantella <gsantella@southhills.edu>
2026-09-16 16:17:39 -0400

    connect static assets for each student and add my own

--- src/routes/guido/routes.js
+++ src/routes/guido/routes.js
@@ -6,4 +6,8 @@ router.get("/", (req, res) => {
   res.send("Guido Santella!")
 });
 
+router.get("/refresh", (req, res) => {
+  res.send("I didn't restart the server")
+});
+
 export default router;
