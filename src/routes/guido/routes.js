ec5af30f97d7231e1492e8424c747e8ca5f4220a
gsantella <gsantella@southhills.edu>
2026-09-16 15:54:35 -0400

    make changes

--- src/routes/guido/routes.js
+++ src/routes/guido/routes.js
@@ -6,4 +6,14 @@ router.get("/", (req, res) => {
   res.send("Guido Santella!")
 });
 
+router.get("/hotsauce", (req, res) => {
+  const typeOfHotSauce = req.query.type
+  if (typeOfHotSauce) {
+    res.send(`You must like ${typeOfHotSauce} hot sauce!`)
+  } else {
+    res.send("Query String Please!")
+  }
+ 
+});
+
 export default router;
