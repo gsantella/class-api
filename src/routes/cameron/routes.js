618ec02dbcc4cea4837f01de1cf84e0b3cd073b7
clynch37 <camlynch314@gmail.com>
2026-09-21 14:43:15 -0400

    Add API button to cl/index

--- src/routes/cameron/routes.js
+++ src/routes/cameron/routes.js
@@ -3,7 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.send("Cameron Lynch.")
+    res.send("You've reached the Cameron Lynch API, how may I help you?")
 });
 
 
